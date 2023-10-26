fba3bbfe726bcc8a2df4ff22174bd819cee64006
Yasas <118436384+Yasashari@users.noreply.github.com>
2023-10-26 05:48:50 +0100

    Create Conditional.sol

---
+++ Conditional.sol
@@ -0,0 +1,55 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity 0.8.17;
+
+contract AssemblyVar{
+
+    function assemIf(uint x) public pure returns(uint z) {
+        assembly {
+           if lt(x , 10) {z := 99}
+        }
+    }
+
+
+    function assemSwitch(uint x) public pure returns (uint z) {
+        assembly {
+            switch x
+            case 1 {
+                z:=10
+            }
+            case 2 {
+                z:=20
+            }
+
+            default {
+                z:=100
+            }
+        }
+    }
+
+
+    function min(uint x , uint y) public pure returns(uint z) {
+        z = y ;
+        assembly {
+            if lt(x , y) {z:=x}
+        }
+
+    }
+
+    
+    function max(uint x , uint y) public pure returns(uint z) {
+       
+
+        assembly {
+            switch gt(x , y) 
+            case 1 {
+                z:= x
+            }
+            default {
+                z:= y
+            }
+            
+        }
+    }
+  
+}
